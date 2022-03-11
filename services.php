954f5e64d303f6ca5800b292e53a999f9ba93508
Tigran Gasparyan <91489092+TigranGasparyan1@users.noreply.github.com>
2022-03-11 06:55:52 -0800

    Add files via upload

--- services.php
+++ services.php
@@ -1,9 +1,15 @@
 <?php require "blocks/header.php"?>
 
-<a href="https://t.me/CryptoToUsdPrice_bot"><img class="servies-img" src="img/bot.svg" alt=""></a>
+<img class="servies-img" src="img/icon.ico" alt="">
 
+<div class="servise_div">
+<h1 class="header_servies">Exchange App</h1>
 
-<p class="servies-a"><a href="https://t.me/CryptoToUsdPrice_bot">Crypto Price</a></p><p class="p-servies"> Bot for know price of toncoin, bitcoin, ethereum and solana </p>
+<p><strong> Exchange App</strong> has the same algorithm as Exchange Bot. In this app you usually can convert almost everything</p>
+<p>Supported whit Windows</p>
 
+<h3><a href="download/Exchange App.exe" download="">Dwonload App</a></h3>
+<h3><a href="download/Exchange App.torrent" download="">Dwonload .torrent</a></h3>
 
+</div>
 <?php require "blocks/footer.php"?>
